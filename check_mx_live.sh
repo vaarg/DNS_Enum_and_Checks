4c75474f39f9fa213214879912b9bee0162acb8c
vaarg <104344158+vaarg@users.noreply.github.com>
2023-12-11 13:16:00 +1100

    Update check_mx_live.sh

--- check_mx_live.sh
+++ check_mx_live.sh
@@ -10,30 +10,19 @@ DOMAIN=$1
 
 # Get MX records
 echo "Getting MX records for $DOMAIN..."
-MX_RECORDS=$(dig mx $DOMAIN +short)
+MX_RECORDS=$(dig mx $DOMAIN +short | awk '{print $2}')
 
 # Loop through each MX record
-for mx in $MX_RECORDS; do
-    # Extract MX domain
-    MX_DOMAIN=$(echo $mx | awk '{print $2}')
-
-    # If MX_DOMAIN is empty, skip to the next record
-    if [ -z "$MX_DOMAIN" ]; then
-        continue
-    fi
-
+for MX_DOMAIN in $MX_RECORDS; do
     echo "Looking up IPs for $MX_DOMAIN..."
-    
+
     # Get IPs for MX domain
     IPs=$(dig +short $MX_DOMAIN)
 
     # Check if each IP is alive
     for ip in $IPs; do
         echo "Pinging $ip..."
-        ping -c 1 $ip > /dev/null 2>&1
-
-        # Check the exit status of ping
-        if [ $? -eq 0 ]; then
+        if ping -c 1 $ip &> /dev/null; then
             echo "$ip is alive."
         else
             echo "$ip is not responding."
